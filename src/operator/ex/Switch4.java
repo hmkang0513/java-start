d96e8d425a39d7a73db80f9c9490e39ffe9a9acd
kanghm <kanghm0513@khu.ac.kr>
2026-01-06 17:12:22 +0900

    29. switch문

---
+++ src/operator/ex/Switch4.java
@@ -0,0 +1,16 @@
+package operator.ex;
+
+public class Switch4 {
+    // 자바 14의 새로운 switch
+    public static void main(String[] args) {
+        int grade = 2;
+
+        int coupon = switch (grade) {
+            case 1 -> 1000;
+            case 2 -> 2000;
+            case 3 -> 3000;
+            default -> 500;
+        };
+        System.out.println("발급받은 쿠폰 : " + coupon);
+    }
+}
