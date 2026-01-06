25cb65ee5543f2fc8e5fdbf5af95f9cc2110f45d
kanghm <kanghm0513@khu.ac.kr>
2026-01-06 13:36:35 +0900

    15. 문제와 풀이

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx1 {
+    public static void main(String[] args){
+        // 반복해서 나오는 숫자 4, 3를 다른 숫자로 한번에 변경할 수 있도록 num1, num2를 사용해서 변경하기
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ src/variable/ex/VarEx2.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        /*  num1을 선언하고 10을 할당
+            num2를 선언하고 20을 할당
+            두 변수의 합을 구하고 sum에 저장
+            sum 변수의 값을 출력
+         */
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
