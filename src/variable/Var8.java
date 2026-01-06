79bed5d450bf20a30cad108978460659ea4937c4
kanghm <kanghm0513@khu.ac.kr>
2026-01-06 13:18:03 +0900

    13. 변수 타입2

---
+++ src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // -2,146,483,648 ~ 2,147483,637 약 20억
+
+        long l = 9223372036854775807L; // 대문자 L을 뒤에 붙임
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+        // 메모리를 적게 사용하면 적은 숫자를 표현할 수 있고 메모리를 많이 사용하면 큰 숫자를 표현할 수 있다.
+    }
+}
