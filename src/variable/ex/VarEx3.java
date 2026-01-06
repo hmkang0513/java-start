b2e80d726c308f251296c8eb28de88b366a9549a
kanghm <kanghm0513@khu.ac.kr>
2026-01-06 13:39:56 +0900

    15. 문제와 풀이

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,14 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args){
+        /*
+        long 타입의 변수를 선언 후 백억으로 초기화 한 후 출력
+        boolean 타입의 변수를 선언 후 true로 초기화
+         */
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+    }
+}
