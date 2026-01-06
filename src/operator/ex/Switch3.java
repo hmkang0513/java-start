5d0ee47ab5102fc422f0a6668dcb731ff7d61a2d
kanghm <kanghm0513@khu.ac.kr>
2026-01-06 17:10:06 +0900

    29. switch문

---
+++ src/operator/ex/Switch3.java
@@ -0,0 +1,24 @@
+package operator.ex;
+
+public class Switch3 {
+    //break가 없는 경우
+    public static void main(String[] args){
+        int grade = 2;
+
+        int coupon;
+        switch(grade){
+            case 1:
+                coupon = 1000;
+                break;
+
+            case 2:
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+        }
+        System.out.println("발급받은 쿠폰 : " + coupon);
+        // break가 없는 경우에는 그 다음 것도 실행이 된다.
+    }
+}
