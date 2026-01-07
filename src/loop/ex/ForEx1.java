f663fbb54df45e2da6ae3030ec302356ee57898d
kanghm <kanghm0513@khu.ac.kr>
2026-01-07 15:02:06 +0900

    42. 문제와 풀이1

---
+++ src/loop/ex/ForEx1.java
@@ -0,0 +1,11 @@
+package loop.ex;
+
+public class ForEx1 {
+    public static void main(String[] args) {
+        System.out.println();
+        int count = 0;
+        for(count=1; count<=10; count++){
+            System.out.println(count);
+        }
+    }
+}
