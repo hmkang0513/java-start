a1d9e9d601d831100cbbae726fc9c710c62d8175
kanghm <kanghm0513@khu.ac.kr>
2026-01-08 17:46:53 +0900

    68. 문제와 풀이

--- src/array/ex/ArrayEx8.java
+++ src/array/ex/ArrayEx8.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ArrayEx8 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("학생 수를 입력하세요.: );
+        System.out.print("학생 수를 입력하세요.:" );
 
         int studentCount = scan.nextInt();
 
